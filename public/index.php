e30ec69d8cedb5e5b53b708877e9a4b4bb931201
paly2 <upsilon@langg.net>
2017-08-03 17:05:11 +0200

    Basic setup for the Bullet PHP micro-framework This is my very first php script :smiley:

--- public/index.php
+++ public/index.php
@@ -1,3 +1,23 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
 
-echo "test? It's okay!!!";
+try {
+	$db = new PDO('mysql:host=localhost;dbname=umabis;charset=utf8', 'umabis', 'iuR03JPGFaLsayL5');
+} catch (Exception $e) {
+	die('Error while getting access to database: ' . $e->getMessage());
+}
+
+$app = new Bullet\App();
+
+// This associative array contains the user sessions tokens
+$sessions = array();
+
+$app->path('api', function($request) use($app) {
+	$app->path('authenticate', function() use($app) {
+		$app->post(function($request) {
+			//return 'Authentication: ' . $request->post('name') . ' - ' . $request->post('hash');
+		});
+	});
+});
+
+echo $app->run(new Bullet\Request());
