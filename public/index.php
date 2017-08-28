7992c9715a62123f94dcb63b731f03efa65e716f
upsilon <upsilon@langg.net>
2017-08-28 15:50:02 +0200

    Implement 'hello' and 'register' commands

--- public/index.php
+++ public/index.php
@@ -32,16 +32,49 @@ function command_wrapper($request, $action) {
 }
 
 $app->path('api', function($request) use($app, $db) {
+	$app->path('register', function($request) use($app, $db) {
+		$app->post(function($request) use($db) {
+			$name = $request->post('name');
+			$hash = $request->post('hash');
+			$email = $request->post('e-mail');
+			$is_email_public = (int) $request->post('is_email_public', '1');
+			$ip_address = $request->post('ip_address');
+
+			// Check request validity
+			if (!$hash || !$name || !$email || !$ip_address)
+				return '012';
+
+			// Make sure that the account does not already exists
+			$req = $db->prepare('SELECT * FROM users WHERE nick = ?');
+			$req->execute(array($name));
+			if ($req->rowCount() != 0)
+				return '015';
+
+			// Make sure that the IP is not blacklisted
+			$blacklist_entry_ip = Blacklist\get_entry_ip($ip_address);
+			if (!empty($blacklist_entry_ip))
+				return '005' . json_encode($blacklist_entry_ip);
+
+			// Register the user
+			$req = $db->prepare('INSERT INTO users(nick, email, is_email_public, password_hash) VALUES(:nick, :email, :is_email_public, :hash)');
+			$req->execute(array(
+				'nick' => $name,
+				'email' => $email,
+				'is_email_public' => $is_email_public,
+				'hash' => $hash
+			));
+
+			$req = $db->prepare('INSERT INTO user_IPs(nick, IP_address) VALUES(?, ?)');
+			$req->execute(array($name, $ip_address));
+
+			return '000';
+		});
+	});
 	$app->path('authenticate', function($request) use($app, $db) {
 		$app->post(function($request) use($db) {
-			/*$hash = $request->post('hash');
+			$hash = $request->post('hash');
 			$name = $request->post('name');
-			$ip_address = $request->post('ip_address');*/
-			$hash = $_POST['hash'];
-			$name = $_POST['name'];
-			$ip_address = $_POST['ip_address'];
-
-			echo "$hash<br>$name<br>$ip_address<br>";
+			$ip_address = $request->post('ip_address');
 
 			// Check request validity
 			if (!$hash || !$name || !$ip_address) {
@@ -75,6 +108,8 @@ $app->path('api', function($request) use($app, $db) {
 
 			// TODO: check the number of failing authentication attempts
 
+			// TODO: add the IP to the user IPs
+
 			// Create the session
 			$token = Session\create_session($user['nick']);
 			if ($token === 1) {
@@ -92,11 +127,18 @@ $app->path('api', function($request) use($app, $db) {
 			});
 		});
 	});
-	$app->patch('ping', function($request) use ($app, $db) {
+	$app->path('ping', function($request) use ($app, $db) {
 		$app->post(function($request) use($db) {
 			return command_wrapper($request, function($name) {
 				// Do nothing more :p
 			});
+		});
+	});
+	$app->path('hello', function($request) use ($app) {
+		$app->get(function($request) {
+			return '000' . json_encode(array(
+				'SESSION_EXPIRATION_TIME' => SESSION_EXPIRATION_TIME
+			));
 		});
 	});
 });
